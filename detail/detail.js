0a5c1e77eef08fcf17d0240dae81c14552011531
Victor Caruso <vic.caruso@gmail.com>
2021-12-13 12:46:58 -0800

    dog details page working

--- detail/detail.js
+++ detail/detail.js
@@ -8,3 +8,13 @@ const dogDetailContainer = document.getElementById('dog-detail-container');
 // use the id to fetch the dog
 // render and append this dog's details to the container
 
+window.addEventListener('load', async() => {
+    const params = new URLSearchParams(window.location.search);
+    const dogID = params.get('id');
+
+    const dogData = await getDog(dogID);
+
+    const dogEl = renderDogDetail(dogData);
+
+    dogDetailContainer.append(dogEl);
+});
